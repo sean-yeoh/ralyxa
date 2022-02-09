19f0463a1a4602dc3ddefaeb2d8a5af2bed4edc9
Sean Yeoh <sean.yeoh@hotmail.com>
2022-02-10 04:01:46 +0800

    Add idle command

--- lib/ralyxa/response_entities/directives/alexa_presentation_language.rb
+++ lib/ralyxa/response_entities/directives/alexa_presentation_language.rb
@@ -4,9 +4,16 @@ module Ralyxa
       class AlexaPresentationLanguage
         class << self
 
-          def render(datasources: nil, document:, speech: nil, session_attributes: nil, response_builder: Ralyxa::ResponseBuilder)
+          def render(datasources: nil, document:, idle_minutes: 10, speech: nil, session_attributes: nil, response_builder: Ralyxa::ResponseBuilder)
             directive = build_directive(document, datasources)
-            json = response_builder.build(build_options(directive, speech))
+            options_hash = build_options(
+              directive: directive,
+              speech: speech,
+              document: document,
+              idle_delay: idle_minutes * 60000 # milliseconds
+            )
+
+            json = response_builder.build(options_hash)
             response = JSON.parse(json)
 
             response[:sessionAttributes] = session_attributes if session_attributes
@@ -15,9 +22,20 @@ module Ralyxa
 
           private
 
-          def build_options(directive, speech)
+          def build_options(directive:, document:, idle_delay:, speech:)
+            idle_directive = {
+              type: 'Alexa.Presentation.APL.ExecuteCommands',
+              token: document,
+              commands: [
+                {
+                  type: "Idle",
+                  delay: idle_delay
+                }
+              ]
+            }
+
             {}.tap do |option|
-              option[:directives] = [directive]
+              option[:directives] = [directive, idle_directive]
               option[:end_session] = false
               option[:response_text] = speech if speech
             end
@@ -26,6 +44,7 @@ module Ralyxa
           def build_directive(document, datasources)
             {
               type: "Alexa.Presentation.APL.RenderDocument",
+              token: document,
               document: {
                 src: "doc://alexa/apl/documents/#{document}",
                 type: "Link"
